eeee02c5b30056595232a3fe08d0c0f2e42bedf6
Dmytro Kononchuk (PC) <dimakononchuk12@gmail.com>
2025-11-12 16:35:10 +0200

    refactor zendeskWidget

--- pages/Widget/ZendeskWidget.js
+++ pages/Widget/ZendeskWidget.js
@@ -3,18 +3,9 @@
 export class ZendeskWidget {
     constructor(page) {
         this.page = page;
-        this.frame = page.locator('#webWidget');
-        this.title = page.locator('[data-testid="widget-title"]');
+        this.frame = page.locator('#webWidget')
     }
 
-    async waitForLoad() {
-       
-        // чекаємо, поки елемент всередині iframe стане видимим
-        await this.title.waitFor({ state: 'visible', timeout: 10000 });
-    }
-    getTitle(){
-        return this.title;
-    }
     getWidget(){
         return this.frame;
     }
