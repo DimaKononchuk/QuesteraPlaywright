10712790b29a375bee688c4747afbc9f8aca2666
Dmytro Kononchuk (PC) <dimakononchuk12@gmail.com>
2025-11-18 12:39:38 +0200

    add daily locators

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -9,6 +9,8 @@ export class HomePage{
         
         this.startedBlock=page.locator('div.started-main-wrapper');
         this.startedItem=page.locator('div.started-item')
+        this.dailyBox=page.locator('div.daily');
+        this.dailyItem=page.locator('div.daily__item').first();
     
     }
 
@@ -21,6 +23,13 @@ export class HomePage{
     }
     getStartedItem(item){
         return this.startedItem.filter({hasText:item});
+    }
+
+    getDailyBox(){
+        return this.dailyBox;
+    }
+    getDailyItem(){
+        return this.dailyItem;
     }
 
 
